db54f27fb8e97b05db05209dc1704ac6c0f21af2
joyjsmun <joy.js.mun@gmail.com>
2022-07-29 22:12:22 +0900

    ADD: deploy.js

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,29 +1,32 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const {ethers} = require("hardhat");
+require("dotenv").confit({path:".env"});
+const {CRYPTO_DEVS_NFT_CONTRACT_ADDRESS} = require("../constants");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+async function main(){
+  //Address of the nft contract
+  const cryptoDevsNFTContract = CRYPTO_DEVS_NFT_CONTRACT_ADDRESS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  // A ContractFactory in ehters.js is an abstraction used to deploy new smart contracts,
+  //so cryptoDevsTokenConctract here is a factory for instance of our CryptoDevToken contract
+  const cryptoDevsTokenContract = await ethers.getContractFactory(
+    "CryptoDevToken"
+  );
+  
+  //deploy the contract
+    const deployedCryptoDevsTokenContract = await cryptoDevsTokenContract.deploy(
+      cryptoDevsNFTContract
+    )
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    console.log(
+      "CryptoDevs Token Contract Address :",
+      deployedCryptoDevsTokenContract.address
+    );
 
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(( ) => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  })
